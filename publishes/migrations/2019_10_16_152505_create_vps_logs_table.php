120a7b0e6f52c04aba578b6fe53f8cbe0311f110
B14ckP4nd4 <B14ckP4nd4@protonmail.com>
2019-10-19 14:05:45 +0200

    add description to log model

--- publishes/migrations/2019_10_16_152505_create_vps_logs_table.php
+++ publishes/migrations/2019_10_16_152505_create_vps_logs_table.php
@@ -1,41 +1,42 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-class CreateVPSLOGTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class CreateVPSLOGTable extends Migration
     {
-        Schema::create('vps_logs', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('server_id');
-            $table->unsignedBigInteger('vps_id');
-            $table->unsignedBigInteger('task_id')->nullable();
-            $table->string('task');
-            $table->string('status');
-            $table->timestamp('date');
-            $table->timestamps();
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            Schema::create('vps_logs', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('server_id');
+                $table->unsignedBigInteger('vps_id');
+                $table->unsignedBigInteger('task_id')->nullable();
+                $table->string('task');
+                $table->string('status');
+                $table->text('description');
+                $table->timestamp('date');
+                $table->timestamps();
 
 
-            $table->foreign('server_id')->references('id')->on('servers');
-            $table->foreign('vps_id')->references('id')->on('vps');
-        });
+                $table->foreign('server_id')->references('id')->on('servers');
+                $table->foreign('vps_id')->references('id')->on('vps');
+            });
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::dropIfExists('v_p_s_l_o_g');
+        }
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('v_p_s_l_o_g');
-    }
-}
